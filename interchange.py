64f9c593143fc4185e1f253e810db84c00ff545f
paul20211 <94079794+Paul20211@users.noreply.github.com>
2023-03-13 15:32:36 +0300

    Add files via upload

---
+++ interchange.py
@@ -0,0 +1,13 @@
+# Function to check if a number is even or odd
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+
+# Example usage
+number = 10
+check_even_or_odd(number)
+
+number = 7
+check_even_or_odd(number)
